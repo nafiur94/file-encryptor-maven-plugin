fb27808b1b9ed11908a0880b91bcec387df32ea0
Kazi Nafiur Hoque <nafiur.hoque@dsinnovators.com>
2024-02-05 20:06:51 +0600

    Temp file removed issue fixed

--- src/main/java/org/example/FileDecryptorMavenPlugin.java
+++ src/main/java/org/example/FileDecryptorMavenPlugin.java
@@ -11,6 +11,7 @@ import org.example.utility.FileReader;
 import org.example.utility.FolderEncryptorDecryptor;
 
 import java.io.File;
+import java.io.IOException;
 
 @Mojo(name = "FileDecryptor", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class FileDecryptorMavenPlugin extends AbstractMojo {
@@ -19,25 +20,38 @@ public class FileDecryptorMavenPlugin extends AbstractMojo {
     @Parameter(defaultValue = "-Y0URSTR0NGPA$$-")
     private String secret;
 
+    @Parameter(defaultValue = "")
+    private String directory;
+
     @Parameter(defaultValue = "${project}")
     private MavenProject project;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         getLog().info("File Decryptor Plugin Started");
+        FileReader fileReader = new FileReader();
+        String srcDir = project.getBasedir().getAbsolutePath() + "/" + directory;
+        String temDir = srcDir + tempFolder;
+
         try {
-            String srcDir = project.getBasedir().getAbsolutePath() + "/src";
-            String temDir = srcDir + tempFolder;
             getLog().info("Source Directory : " + srcDir);
             getLog().info("Temp Directory : " + temDir);
             FolderEncryptorDecryptor encryptorDecryptor = new FolderEncryptorDecryptor(secret);
             encryptorDecryptor.decrypt(srcDir, temDir);
-            FileReader fileReader = new FileReader();
             fileReader.copyFolder(new File(temDir), new File(srcDir));
-            fileReader.deleteFolder(new File(temDir).getParentFile());
         } catch (Exception e) {
             e.printStackTrace();
             throw new MojoExecutionException("Unable to Decrypt the file");
+        } finally {
+            File tempFile = new File(temDir);
+            if (tempFile.exists()) {
+                try {
+                    fileReader.deleteFolder(tempFile.getParentFile());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    throw new MojoExecutionException("Unable to Remove temporary files");
+                }
+            }
         }
         getLog().info("File Decryptor Plugin Ended");
     }
--- src/main/java/org/example/FileEncryptorMavenPlugin.java
+++ src/main/java/org/example/FileEncryptorMavenPlugin.java
@@ -11,6 +11,7 @@ import org.example.utility.FileReader;
 import org.example.utility.FolderEncryptorDecryptor;
 
 import java.io.File;
+import java.io.IOException;
 
 @Mojo(name = "FileEncryptor", defaultPhase = LifecyclePhase.INSTALL)
 public class FileEncryptorMavenPlugin extends AbstractMojo {
@@ -19,25 +20,39 @@ public class FileEncryptorMavenPlugin extends AbstractMojo {
     @Parameter(defaultValue = "-Y0URSTR0NGPA$$-")
     private String secret;
 
+    @Parameter(defaultValue = "")
+    private String directory;
+
     @Parameter(defaultValue = "${project}")
     private MavenProject project;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         getLog().info("File Encryptor Plugin Started");
+        FileReader fileReader = new FileReader();
+        String srcDir = project.getBasedir().getAbsolutePath() + "/" + directory;
+        String temDir = srcDir + tempFolder;
+
         try {
-            String srcDir = project.getBasedir().getAbsolutePath() + "/src";
-            String temDir = srcDir + tempFolder;
             getLog().info("Source Directory : " + srcDir);
             getLog().info("Temp Directory : " + temDir);
             FolderEncryptorDecryptor encryptorDecryptor = new FolderEncryptorDecryptor(secret);
             encryptorDecryptor.encrypt(srcDir, temDir);
-            FileReader fileReader = new FileReader();
             fileReader.copyFolder(new File(temDir), new File(srcDir));
             fileReader.deleteFolder(new File(temDir).getParentFile());
         } catch (Exception e) {
             e.printStackTrace();
             throw new MojoExecutionException("Unable to Encrypt the file");
+        } finally {
+            File tempFile = new File(temDir);
+            if (tempFile.exists()) {
+                try {
+                    fileReader.deleteFolder(tempFile.getParentFile());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    throw new MojoExecutionException("Unable to Remove temporary files");
+                }
+            }
         }
         getLog().info("File Encryptor Plugin Ended");
     }
